1179e6bc26cdd23f44f180febbf5339ce0e61ba5
Wensheng Wang <wenshengwang@gmail.com>
2017-05-26 21:05:47 +0800

    copy fonts

--- fonts/copy_fonts.bat
+++ fonts/copy_fonts.bat
@@ -1,10 +1,32 @@
-rem for western
-copy %SystemRoot%\fonts\verdana.ttf .
-copy %SystemRoot%\fonts\tahoma.ttf .
-rem for symbol
-copy %SystemRoot%\fonts\symbol.ttf .
-rem for Chinese
-copy %SystemRoot%\fonts\msyh.ttc .
-copy %SystemRoot%\fonts\simsun.ttc .
-copy %SystemRoot%\fonts\simkai.ttf .
-copy %SystemRoot%\fonts\simfang.ttf .
+rem these should be present on every win 10
+copy %SystemRoot%\fonts\cambria.ttc
+copy %SystemRoot%\fonts\consola.ttf
+copy %SystemRoot%\fonts\cour.ttf
+copy %SystemRoot%\fonts\segoeui.ttf
+copy %SystemRoot%\fonts\seguisym.ttf
+copy %SystemRoot%\fonts\tahoma.ttf
+copy %SystemRoot%\fonts\verdana.ttf
+rem uncomment when you have installed more language packs
+rem copy %SystemRoot%\fonts\msyh.ttc .
+rem copy %SystemRoot%\fonts\simsun.ttc .
+rem copy %SystemRoot%\fonts\simkai.ttf .
+rem copy %SystemRoot%\fonts\simfang.ttf .
+rem copy %SystemRoot%\fonts\batang.ttc
+rem copy %SystemRoot%\fonts\gulim.ttc
+rem copy %SystemRoot%\fonts\kaiu.ttf
+rem copy %SystemRoot%\fonts\malgun.ttf
+rem copy %SystemRoot%\fonts\meiryo.ttc
+rem copy %SystemRoot%\fonts\meiryob.ttc
+rem copy %SystemRoot%\fonts\mingliu.ttc
+rem copy %SystemRoot%\fonts\msgothic.ttc
+rem copy %SystemRoot%\fonts\msjh.ttc
+rem copy %SystemRoot%\fonts\msjhbd.ttc
+rem copy %SystemRoot%\fonts\msmincho.ttc
+rem copy %SystemRoot%\fonts\msyh.ttc
+rem copy %SystemRoot%\fonts\simfang.ttf
+rem copy %SystemRoot%\fonts\simkai.ttf
+rem copy %SystemRoot%\fonts\simsun.ttc
+rem copy %SystemRoot%\fonts\symbol.ttf
+rem copy %SystemRoot%\fonts\YuGothB.ttc
+rem copy %SystemRoot%\fonts\YuGothM.ttc
+rem copy %SystemRoot%\fonts\YuGothR.ttc
